6ce14dc03b1e2c4356dac751b2bdc2a7a4b51279
Jim Hessin <grillbrick@gmail.com>
2015-11-29 13:42:54 -0700

    - Initial Commit.

---
+++ app/src/main/java/com/grillbrickstudios/modalist/model/Table.java
@@ -0,0 +1,14 @@
+package com.grillbrickstudios.modalist.model;
+
+/**
+ * Created by jhess on 11/29/2015 for ModaList.
+ * A list of variables used to create the database.
+ */
+public final class Table {
+	public static final String DB_NAME = "ListDb.db";
+	public static final String TBL_NAME = "ListTable";
+	public static final String C_ID = "_id";
+	public static final String C_LISTNAME = "List";
+	public static final String C_ITEMNAME = "Item";
+	public static final String C_CHECKED = "Checked";
+}
